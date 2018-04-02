d7e4d576826a72ece4ff71b0b4949e968b53b6d5
Ic3Sandy <monkey_king2@hotmail.com>
2018-04-02 17:57:41 +0700

    Add new server and code gen qr

--- https-express.js
+++ https-express.js
@@ -1,17 +1,20 @@
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+var privateKey  = fs.readFileSync('hacksparrow-key.pem');
+var certificate = fs.readFileSync('hacksparrow-cert.pem');
 
 var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
 var app = express();
 
 // your express configuration here
+app.get('/', function (req, res) {
+    res.sendFile(__dirname+'/view/main.html');
+});
 
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(8080);
-httpsServer.listen(8443);
+httpsServer.listen(5000);
--- server.js
+++ server.js
@@ -1,32 +1,38 @@
-var express = require('express');
-var app = express();
+var https = require('https')
+var http = require('http')
+var pem = require('pem')
+var express = require('express')
+var app = express()
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.get('/main', function (req, res) {
-    res.sendFile(__dirname+'/view/main.html');
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+'/view/main.html');
 });
 
 app.get('/test-post', function (req, res) {
-    res.sendFile(__dirname+'/view/test-post.html');
+  res.sendFile(__dirname+'/view/test-post.html');
 });
 
 app.get('/test-sendfile', function (req, res) {
-    res.download(__dirname+'/view/main.html', 'namefile.html');
+  res.download(__dirname+'/view/main.html', 'namefile.html');
+});
+
+app.get('/camera', function (req, res) {
+  res.sendFile(__dirname+'/view/scanqr.html');
 });
 
 app.post('/main', function (req, res) {
-    res.redirect(200, 'http://localhost:8080/main');
-    console.log(req.body.user.name);
-    console.log(req.body.user.email);
+  res.redirect(200, 'https://192.168.1.4:5000/');
+  console.log(req.body.user.name);
+  console.log(req.body.user.email);
 });
 
-var server = app.listen(5000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log("Example app listening at http://%s:%s", host, port);
-
-});
+http.createServer(app).listen(8000)
+pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
+  if (err) {throw err}
+  var option = { key: keys.serviceKey, cert: keys.certificate }
+  https.createServer(option, app).listen(5000)
+})
