b3de3aa84ac12329344e25c8d4f61e0db1cc1750
Ic3Sandy <monkey_king2@hotmail.com>
2018-04-08 18:39:13 +0700

    Add session to page home and function checkSession at db_server

--- db_server.js
+++ db_server.js
@@ -19,7 +19,7 @@ con.connect(function(err) {
         console.log("[db_server] Table drop")
     })
 
-    var sql = "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, acc_num INT, username VARCHAR(255), password VARCHAR(255), balance INT,session VARCHAR(255))";
+    var sql = "CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, acc_num INT, username VARCHAR(255), password VARCHAR(255), balance INT, session VARCHAR(255))";
     con.query(sql, function (err, result) {
         if (err) throw err
         console.log("[db_server] Table created")
@@ -29,7 +29,7 @@ con.connect(function(err) {
     var values = [
         [123, 'foo', 'bar', 500],
         [456, 'alice', 'bob', 500],
-    ];
+    ]
     con.query(sql, [values], function (err, result) {
       if (err) throw err
     //   console.log("Number of records inserted: " + result.affectedRows);
@@ -37,7 +37,7 @@ con.connect(function(err) {
 
 })
 
-exports.checkLogin = function(username, password, callback) {
+exports.checkLogin = function(username, password, session, callback) {
     
     console.log("[db_server checkLogin] Check password!")
     var sql = 'SELECT * FROM users WHERE username = ' + mysql.escape(username)
@@ -50,9 +50,32 @@ exports.checkLogin = function(username, password, callback) {
 
         if (result.length == 0 || result[0].password != password){
             console.log('[db_server checkLogin] Username or Password Incorrect')
-            callback(false)
-        }else if(result[0].password == password){
+            callback(false, null)
+        }
+        else if(result[0].password == password){
             console.log('[db_server checkLogin] Access Granted')
+            var sql = 'UPDATE users SET session = '+mysql.escape(session)+' WHERE username = '+mysql.escape(username)
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log(result.affectedRows + " record(s) updated");
+            })
+            callback(true, session)
+        }
+    })
+}
+
+exports.checkSession = function(session, callback) {
+
+    console.log("[db_server checkSession] Check session!")
+    var sql = 'SELECT * FROM users WHERE session = ' + mysql.escape(session)
+    con.query(sql, function (err, result, fields) {
+        if (err) throw err
+
+        if (result.length == 0){
+            console.log('[db_server checkSession] Session Incorrect')
+            callback(false)
+        }else{
+            console.log('[db_server checkSession] Session Correct')
             callback(true)
         }
     })
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var pem = require('pem')
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var session = require('express-session')
 
 // Own Modules
 var get_ipwifi = require('./modules/get_ipwifi.js')
@@ -20,7 +22,15 @@ else
 app.use(bodyParser.json()) // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 app.engine('html', require('ejs').renderFile) //support for res.render()
+app.use(cookieParser()) // support for req.cookies
 
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'secretjs',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname+'/view/main.html')
@@ -35,28 +45,40 @@ app.post('/login', function (req, res) {
   console.log('[server app.post /login] password: '+req.body.password)
   function checkLogin(check, session){
     if(check){
+      console.log(session)
+      res.cookie('session', session, { maxAge: 1000 * 60}) // 1 minute
       res.redirect(protocol+'://'+ip+':'+port+'/home')
     }else{
       res.redirect(protocol+'://'+ip+':'+port+'/login')
     }
   }
-  db.checkLogin(req.body.username, req.body.password, checkLogin)
+  db.checkLogin(req.body.username, req.body.password, req.sessionID, checkLogin)
 })
 
 app.get('/home', function(req,res){
-	res.render(__dirname+"/view/home.html", {ip:ip});
-});
+  if(Object.keys(req.cookies).length == 0 || !('session' in req.cookies))
+    res.redirect(protocol+'://'+ip+':'+port+'/login')
+  else{
+    function checkSesion(check){
+      if(check)
+        res.render(__dirname+"/view/home.html", {ip:ip})
+      else
+        res.redirect(protocol+'://'+ip+':'+port+'/login')
+    }
+    db.checkSession(req.cookies['session'], checkSesion)
+  }
+})
 
 app.get('/scanqr',function(req,res){
-	res.render(__dirname+"/view/scanqr.html", {ip:ip});
+	res.render(__dirname+"/view/scanqr.html", {ip:ip})
 })
 
 app.get('/genqr',function(req,res){
-	res.render(__dirname+"/view/genqr.html", {ip:ip});
+	res.render(__dirname+"/view/genqr.html", {ip:ip})
 })
 
 app.post('/genqr',function(req,res){
-	res.render(__dirname+"/view/qrcode.html", {ip:ip});
+	res.render(__dirname+"/view/qrcode.html", {ip:ip})
 })
 
 
