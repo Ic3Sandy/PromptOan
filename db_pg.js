65908569fb423c4fd7551f4f4b13cdc5ed429180
Ic3Sandy <monkey_king2@hotmail.com>
2018-04-19 12:30:39 +0700

    Add fucntion create database

--- db_pg.js
+++ db_pg.js
@@ -7,8 +7,17 @@ const client = new Client({
 
 client.connect()
 
-var sql = "CREATE TABLE IF NOT EXISTS users (acc_num INT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), balance INT, session VARCHAR(255))";
-client.query(sql, function (err, result) {
+function create_tb(){
+    var sql = "CREATE TABLE IF NOT EXISTS users (acc_num INT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), balance INT, session VARCHAR(255))";
+    client.query(sql, function (err, result) {
     if (err) throw err
     console.log("[db_server] Table created")
 })
+}
+
+var sql = "DROP TABLE IF EXISTS users"
+con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("[db_server] Table drop")
+})
+
