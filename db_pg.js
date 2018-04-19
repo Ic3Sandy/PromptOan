0b9d78eeba825614c3f36378778cbff3993cd0dd
Ic3Sandy <monkey_king2@hotmail.com>
2018-04-19 14:34:34 +0700

    Test drop table

--- db_pg.js
+++ db_pg.js
@@ -7,31 +7,31 @@ const client = new Client({
 
 client.connect()
 
-function create_tb(){
-    var sql = "CREATE TABLE IF NOT EXISTS users (acc_num INT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), balance INT, session VARCHAR(255))";
-    client.query(sql, function (err, result) {
-    if (err) throw err
-    console.log("[db_server] Table created")
-    insert_info()
-})
-}
+// function create_tb(){
+//     var sql = "CREATE TABLE IF NOT EXISTS users (acc_num INT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), balance INT, session VARCHAR(255))";
+//     client.query(sql, function (err, result) {
+//     if (err) throw err
+//     console.log("[db_server] Table created")
+//     insert_info()
+// })
+// }
 
-function insert_info(){
+// function insert_info(){
 
-    var sql = "INSERT INTO users (acc_num, username, password, balance) VALUES ?";
-    var values = [
-        [123, 'foo', 'bar', 500],
-        [456, 'alice', 'bob', 500],
-    ]
-    con.query(sql, [values], function (err, result) {
-        if (err) throw err
-    //   console.log("Number of records inserted: " + result.affectedRows);
-    })
+//     var sql = "INSERT INTO users (acc_num, username, password, balance) VALUES ?";
+//     var values = [
+//         [123, 'foo', 'bar', 500],
+//         [456, 'alice', 'bob', 500],
+//     ]
+//     client.query(sql, [values], function (err, result) {
+//         if (err) throw err
+//     //   console.log("Number of records inserted: " + result.affectedRows);
+//     })
 
-}
+// }
 
 var sql = "DROP TABLE IF EXISTS users"
-con.query(sql, function (err, result) {
+client.query(sql, function (err, result) {
     if (err) throw err;
     console.log("[db_server] Table drop")
     create_tb()
