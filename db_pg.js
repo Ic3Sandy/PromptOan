c2fa9c35fc9ecbc3374cff88e9378dee3c984b7e
Ic3Sandy <monkey_king2@hotmail.com>
2018-04-19 14:44:42 +0700

    Test insert to table

--- db_pg.js
+++ db_pg.js
@@ -12,23 +12,23 @@ function create_tb(){
     client.query(sql, function (err, result) {
     if (err) throw err
     console.log("[db_server] Table created")
-    // insert_info()
+    insert_info()
 })
 }
 
-// function insert_info(){
+function insert_info(){
 
-//     var sql = "INSERT INTO users (acc_num, username, password, balance) VALUES ?";
-//     var values = [
-//         [123, 'foo', 'bar', 500],
-//         [456, 'alice', 'bob', 500],
-//     ]
-//     client.query(sql, [values], function (err, result) {
-//         if (err) throw err
-//     //   console.log("Number of records inserted: " + result.affectedRows);
-//     })
+    var sql = "INSERT INTO users (acc_num, username, password, balance) VALUES ?";
+    var values = [
+        [123, 'foo', 'bar', 500],
+        [456, 'alice', 'bob', 500],
+    ]
+    client.query(sql, [values], function (err, result) {
+        if (err) throw err
+    //   console.log("Number of records inserted: " + result.affectedRows);
+    })
 
-// }
+}
 
 var sql = "DROP TABLE IF EXISTS users"
 client.query(sql, function (err, result) {
