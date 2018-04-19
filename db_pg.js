22b1c9cb0464aab77e47cae443d20f8acd0931d6
Ic3Sandy <monkey_king2@hotmail.com>
2018-04-19 15:43:06 +0700

    Test CheckLogin 4

--- db_pg.js
+++ db_pg.js
@@ -32,7 +32,7 @@ client.query(sql, function (err, result) {
 exports.checkLogin = function(username, password, session, callback) {
     
     console.log("[db_pg checkLogin] Check password!")
-    var sql = 'SELECT * FROM users WHERE username = ' + $1
+    var sql = `SELECT * FROM users WHERE username = ${username}`
     client.query(sql, function (err, result, fields) {
         if (err) throw err
 
