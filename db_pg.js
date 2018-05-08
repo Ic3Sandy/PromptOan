7657c5576625ad4f5bc258d3e2d3881586feb42c
Ic3Sandy <monkey_king2@hotmail.com>
2018-05-08 23:39:44 +0700

    Fix bug mb_pg 4

--- db_pg.js
+++ db_pg.js
@@ -54,8 +54,7 @@ exports.checkSession = function(session, callback) {
             callback(false, null, null, null)
 
         else{
-            console.log(result['rows'][0])
-            callback(true, result['rows'][0].username, result['rows'][0].acc_num, result['rows'][0].balance)
+            callback(true, result['rows'][0]['username'], result['rows'][0]['acc_num'], result['rows'][0]['balance'])
         }
             
         
@@ -75,7 +74,7 @@ exports.transaction = function(session, payer, balance, acc_num, amount, callbac
     }
     client.query('SELECT * FROM users WHERE acc_num = $1', [acc_num], function (err, result, fields) {
         if (err) throw err
-        var balance_payee = result['rows'][0].balance + amount
+        var balance_payee = result['rows'][0]['balance'] + amount
         upBalance(balance_payee, payee)
     })
     callback()
